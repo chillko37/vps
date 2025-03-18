a78fd9ef25fdd54c1c12d9cfe7ac578f35ac7991
chillko37 <chillko37@gmail.com>
2025-03-18 18:39:28 +0700

    Update start-vnc.sh

--- vnc-railway/start-vnc.sh
+++ vnc-railway/start-vnc.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# Khởi động TightVNC server
 tightvncserver :1 -geometry 1280x720 -depth 24
+
+# Giữ container chạy bằng cách tail log hoặc sleep vô hạn
+tail -f /root/.vnc/2bcb9bc4e0ce:1.log
+# Hoặc dùng: 
+# while true; do sleep 1000; done
